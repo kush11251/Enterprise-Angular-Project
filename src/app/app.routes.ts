ce3622bc13539f7d8af8cc64fed7b7eceb7ba671
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 00:56:01 +0530

    feat: integrate src/app/app.routes.ts

---
+++ src/app/app.routes.ts
@@ -0,0 +1,1 @@
+import { Routes } from '@angular/router';import { HomeComponent } from './home/home.component';import { AboutComponent } from './about/about.component';export const appRoutes: Routes = [  { path: '', component: HomeComponent },  { path: 'about', component: AboutComponent }];
